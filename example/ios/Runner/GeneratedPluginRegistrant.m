0ff5fa1c88660e44c7394d4938db00e7d6c08a3f
ndkai <darkspirit909@gmail.com>
2025-07-25 15:11:12 +0700

    add project

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<images_picker/ImagesPickerPlugin.h>)
+#import <images_picker/ImagesPickerPlugin.h>
+#else
+@import images_picker;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ImagesPickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"ImagesPickerPlugin"]];
+}
+
+@end
